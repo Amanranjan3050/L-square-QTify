1e6dd89c09db061e672d0e7438af3959bd6b7dd6
Aman Ranjan <amanranjanppp@gmail.com>
2024-12-03 23:07:30 +0530

    ME_BUILDOUT_QTIFY_V2_MODULE_ONE

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import Section from "./Components/Section/Section";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import FilterSection from "./Components/FilterSection/FilterSection";
 
 function App() {
+  const [topAlbums, setTopAlbums] = useState([]);
+  const [newAlbums, setNewAlbums] = useState([]);
+  const [songsData, setSongsData] = useState([]);
+  const [fliteredSongs, setFilteredSongs] = useState([]);
+
+  const getTopAlbums = async () => {
+    try {
+      let topAlbumsResp = await axios.get("http://qtify-backend-labs.crio.do/albums/top");
+      setTopAlbums(topAlbumsResp.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getNewAlbums = async () => {
+    try {
+      let newAlbumsResp = await axios.get("http://qtify-backend-labs.crio.do/albums/new");
+      setNewAlbums(newAlbumsResp.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const getSongsData = async () => {
+    try{
+      let songsDataResp = await axios.get("http://qtify-backend-labs.crio.do/songs");
+      setSongsData(songsDataResp.data);
+      setFilteredSongs(songsDataResp.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const generateFilteredSongs = (value)=>{
+    let key="";
+    if(value===0){
+      getSongsData();
+    }
+    else if(value===1){
+      key="rock";
+    }
+    else if(value===2){
+      key="pop";
+    }
+    else if(value===3){
+      key="jazz";
+    }
+    else if(value===4){
+      key="blues"
+    }
+
+    let fliteredSongsArray = songsData.filter((songs)=>songs.genre.key===key);
+    setFilteredSongs(fliteredSongsArray);
+  }
+
+  console.log(fliteredSongs);
+
+  const handleTabsChanges = (value)=>{
+    generateFilteredSongs(value);
+  }
+
+  useEffect(() => {
+    
+    getTopAlbums();
+    getNewAlbums();
+    getSongsData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <Section type="album" title="Top Albums" data={topAlbums}/>
+      <Section type="album" title="New Albums" data={newAlbums}/>
+      <FilterSection type="song" title="Songs" fliteredSongs={fliteredSongs} handleTabsChanges={handleTabsChanges}/>
     </div>
   );
 }
